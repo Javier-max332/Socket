f8fda7544c2c47f937eb730b907610138b725a21
Javier Guadalupe Torres Viramontes <mt8966902@gmail.com>
2025-11-11 12:23:03 -0600

    Socket, Cliente - Servidor

---
+++ src/socket/DataClient.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Javier
+ */
+public class DataClient {
+    
+    public static void main(String[] args) {
+        try {
+            Socket sock = new Socket("10.10.13.209", 6013);
+            InputStream in = sock.getInputStream();
+            
+            BufferedReader bin = new BufferedReader
+                    (new InputStreamReader(in));
+            
+            String line;
+            while( (line = bin.readLine()) != null)
+                    System.out.println(line);
+            
+            sock.close();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
---
+++ src/socket/Server.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package socket;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/**
+ *
+ * @author Javier
+ */
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+            
+            while(true) {
+                try (Socket client = sock.accept()) {
+                    PrintWriter pout = new
+                            PrintWriter(client.getOutputStream(), true);
+                    
+                    pout.println("Hola, soy Chito_R20");
+
+                }
+                
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
